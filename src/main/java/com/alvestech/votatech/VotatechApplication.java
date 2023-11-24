f09eaba6df990d3da50affc831d9396b86c5dbd3
unknown <j.everton.ti.com>
2023-11-23 21:07:33 -0300

    Inicia Projeto

---
+++ src/main/java/com/alvestech/votatech/VotatechApplication.java
@@ -0,0 +1,13 @@
+package com.alvestech.votatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotatechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvestech/votatech/VotatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvestech.votatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
